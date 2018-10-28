b36e7c395d5710290b37eb6fafaa84f1818033af
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-10-28 01:35:38 -0500

    adding custom code

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server, { origins: '*:*'});
+
+app.get('/',function(req, res){
+	res.status(200).send("Hola mundos!");
+});
+
+
+io.on('connection', function(socket){
+	console.log('Alguien se conectó con el socket!');
+});
+
+
+server.listen(process.ENV.PORT, function() {
+	console.log('listing 8080 port and server running');
+});
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
